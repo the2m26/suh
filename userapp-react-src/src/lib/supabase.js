2553a0d4be75a999c9081d37999b42db1308f225
the2m26 <the2m26@gmail.com>
2026-07-28 06:37:24 +0800

    Delete userapp-react-src/src directory

--- userapp-react-src/src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://ndbhgzohmjumicziefnr.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kYmhnem9obWp1bWljemllZm5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NTE3OTYsImV4cCI6MjA5OTQyNzc5Nn0.6iDl7omVwM4_cp5zRFE-2lnl_Y6CBgTWO6JUPxmqu-g';
-
-export const sb = createClient(SUPABASE_URL, SUPABASE_KEY);
